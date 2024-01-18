dc91990e17cdac08e5224bce6eeb359a98812fea
Oren <xxorenxx@gmail.com>
2024-01-18 17:40:57 +0200

    done most of the stuff

--- Program.cs
+++ Program.cs
@@ -1,103 +1,226 @@
 ﻿using System.Data;
 
 // draw the board
-void DrawBoard(int[,] mineField, Game game)
+static void DrawBoard(int[,] mineField, bool[,] mineFieldVisable)
 {
     Console.Clear();
     Console.Write("  |  ");
-    for (int i = 0; i < game.column; i++)
+    for (int i = 0; i < mineField.GetLength(1); i++)
         Console.Write(i + "  ");
     Console.WriteLine();
 
     Console.Write("--+--");
-    for (int i = 0; i < game.column; i++)
+    for (int i = 0; i < mineField.GetLength(1); i++)
         Console.Write("---");
     Console.WriteLine();
 
-    for (int i = 0; i < game.row; i++)
+    for (int i = 0; i < mineField.GetLength(0); i++)
     {
         Console.Write(i + " |");
-        for (int j = 0; j < game.column; j++)
+        for (int j = 0; j < mineField.GetLength(1); j++)
         {
-            Console.Write("  ");
-            if (mineField[i, j] == (int)content.MINE)
-                Console.Write('X');
+            Console.Write(" ");
+            if (mineFieldVisable[i, j] == false)
+                Console.Write(" #");
             else
-                Console.Write(mineField[i, j]);
+            {
+                if (mineField[i, j] == (int)content.MINE)
+                    Console.Write(" X");
+                else if (mineField[i, j] > 0)
+                    Console.Write("{0,2}", (mineField[i, j]));
+                else
+                    Console.Write(" -");
+            }
         }
         Console.WriteLine();
     }
+    Console.WriteLine();
 }
-// draw board end
+
+bool reveal(int row, int column, ref int[,] mineField, ref bool[,] mineFieldVisable)
+{
+    if (mineField[row, column] == Convert.ToInt16(content.MINE))
+    {   // mine
+        return true;
+    }
+    else if (mineField[row, column] == 0)
+    {   // empty
+        mineFieldVisable[row, column] = true;
+
+        for (int i = Math.Max(row - 1, 0); i <= Math.Min(mineField.GetLength(0) - 1, row + 1); i++)
+            for (int j = Math.Max(column - 1, 0); j <= Math.Min(mineField.GetLength(1) - 1, column + 1); j++)
+            {
+                if (i == row && j == column)
+                    continue;
+                if (mineField[i, j] == 0 && mineFieldVisable[i, j] == false)
+                    reveal(i, j, ref mineField, ref mineFieldVisable);
+                mineFieldVisable[i, j] = true;
+            }
+    }
+    else
+    {   // number
+        mineFieldVisable[row, column] = true;
+    }
+    DrawBoard(mineField, mineFieldVisable);
+    return false;
+}
+
+bool checkWin(int[,] mineField, bool[,] mineFieldVisable)
+{
+    bool win = true;
+    for (int i = 0; i < mineField.GetLength(0); i++)
+        for (int j = 0; j < mineField.GetLength(1); j++)
+        {
+            if (mineFieldVisable[i, j] && mineField[i, j] != Convert.ToInt32(content.MINE))
+            {   // number revealed
+                continue;
+            }
+            else if (mineFieldVisable[i, j] && mineField[i, j] == Convert.ToInt32(content.MINE))
+            {   // mine revealed
+                win = false;
+                break;
+            }
+            else if (mineFieldVisable[i, j] == false && mineField[i, j] != Convert.ToInt32(content.MINE))
+            {   // number unrevealed
+                win = false;
+                break;
+            }
+        }
+    return win;
+}
 
 Game game = new Game();
-game = initGame(game, 9, 9, 9);
+int[,] mineField = null;
+bool[,] mineFieldVisable = null;
+startNewGame(9, 9, 9, ref game, ref mineField, ref mineFieldVisable);
 
-int[,] mineField = new int[game.row, game.column];
-
-// init empty board
-for (int i = 0; i < game.row; i++)
+bool lost = false;
+DrawBoard(mineField, mineFieldVisable);
+while (game.state == status.playing)
 {
-    for (int j = 0; j < game.column; j++)
-    {
-        mineField[i, j] = 0;
-    }
-}
-
-// create the mines
-int x, y, minesToCreate = game.mine;
-Random rng = new Random();
-while (minesToCreate > 0)
-{
+    int row, column;
     do
-    {
-        x = rng.Next(0, game.row);
-        y = rng.Next(0, game.column);
-    } while (mineField[x, y] != (int)content.EMPTY);
-    mineField[x, y] = (int)content.MINE;
+    {    // Get X,Y coordinate from user
+        Console.WriteLine();
+        Console.Write("Choose row number: ");
+        row = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Choose column number: ");
+        column = Convert.ToInt32(Console.ReadLine());
+    } while (row < 0 || row > game.row || column < 0 || column > game.column);
 
-    // do numbers around a mine here
-    for (int i = x - 1; i <= x + 1; i++)
+    lost = reveal(row, column, ref mineField, ref mineFieldVisable);
+    if (lost)
     {
-        if (i < 0 || i >= game.row)
-            continue;
-        for (int j = y - 1; j <= y + 1; j++)
+        game.state = status.stopped;
+
+        for (int i = 0; i < game.row; i++)
+            for (int j = 0; j < game.column; j++)
+                mineFieldVisable[i, j] = true;
+
+        DrawBoard(mineField, mineFieldVisable);
+
+        Console.WriteLine("You lost");
+        char again = tryAgain();
+        if (again == 'y' || again == 'Y')
         {
-            if (j < 0 || j >= game.column)
-                continue;
-            if (i == x && j == y)
-                continue;
-            mineField[i, j]++;
+            game.state = status.playing;
+            startNewGame(game.row, game.column, game.mine, ref game, ref mineField, ref mineFieldVisable);
         }
+        else
+            game.state = status.stopped;
     }
-
-    minesToCreate--;
+    if (checkWin(mineField, mineFieldVisable))
+    {
+        Console.WriteLine("You won!");
+        char again = tryAgain();
+        if (again == 'y' || again == 'Y')
+        {
+            game.state = status.playing;
+            startNewGame(game.row, game.column, game.mine, ref game, ref mineField, ref mineFieldVisable);
+        }
+        else
+            game.state = status.stopped;
+    }
 }
 
-
-void reveal(int[,] mineField, Game game)
+static void startNewGame(int row, int column, int mines, ref Game game, ref int[,] mineField, ref bool[,] mineFieldVisable)
 {
-    Console.WriteLine("\n");
-    Console.Write("Choose row number: ");
-    int row = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Choose column number: ");
-    int column = Convert.ToInt32(Console.ReadLine());
-    mineField[row, column] = mineField[row, column] * -1;
-    DrawBoard(mineField, game);
+    game = initGame(game, row, column, mines);
+
+    mineField = new int[game.row, game.column];
+    mineFieldVisable = new bool[game.row, game.column];
+
+    emptyBoard(game, ref mineField, ref mineFieldVisable);
+    createMines(game, mineField);
+
+    DrawBoard(mineField, mineFieldVisable);
 }
-
-
-DrawBoard(mineField, game);
-
-reveal(mineField, game);
 
 static Game initGame(Game game, int row, int column, int mine)
 {
-    game.row = 9;
-    game.column = 9;
-    game.mine = 9;
-    game.state = status.stopped;
+    game.row = row;
+    game.column = column;
+    game.mine = mine;
+    game.state = status.playing;
     return game;
+}
+
+static char tryAgain()
+{
+    Console.WriteLine("Try again? y/n");
+    char again = Convert.ToChar(Console.ReadLine());
+    while ((again != 'y' && again != 'n' && again != 'Y' && again != 'N'))
+    {
+        Console.WriteLine("invalid, want to play again?");
+        again = Convert.ToChar(Console.Read());
+    }
+    return again;
+}
+
+static void emptyBoard(Game game, ref int[,] mineField, ref bool[,] mineFieldVisable)
+{
+    // init empty board
+    for (int i = 0; i < game.row; i++)
+    {
+        for (int j = 0; j < game.column; j++)
+        {
+            mineField[i, j] = 0;
+            mineFieldVisable[i, j] = false;
+        }
+    }
+}
+
+static void createMines(Game game, int[,] mineField) {   
+    // create the mines
+    int x, y, minesToCreate = game.mine;
+    Random rng = new Random();
+    while (minesToCreate > 0)
+    {
+        do
+        {
+            x = rng.Next(0, game.row);
+            y = rng.Next(0, game.column);
+        }
+        while (mineField[x, y] != (int)content.EMPTY);
+
+        mineField[x, y] = (int)content.MINE;
+
+        // place numbers around the mines
+        for (int i = x - 1; i <= x + 1; i++)
+        {
+            if (i < 0 || i >= game.row)
+                continue;
+            for (int j = y - 1; j <= y + 1; j++)
+            {
+                if (j < 0 || j >= game.column)
+                    continue;
+                if (i == x && j == y)
+                    continue;
+                mineField[i, j]++;
+            }
+        }
+        minesToCreate--;
+    }
 }
 
 struct Game
@@ -119,3 +242,4 @@ enum content
     MINE = 9,
     EMPTY = 0
 }
+
